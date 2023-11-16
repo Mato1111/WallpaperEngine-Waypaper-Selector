fa43b6ffaae03fa6e2a76444c7ef7698db8c8de5
Mato <96366252+Mato1111@users.noreply.github.com>
2023-11-16 01:23:47 -0500

    Create add-to-apps.sh

---
+++ add-to-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+cd /usr/bin/
+sudo wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/Scripts/wallpaper-select
+
+cd /usr/share/applications/
+sudo wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/Scripts/wallpaper-select.desktop
+
+sudo chmod +x /usr/bin/wallpaper-select
+sudo chmod +x /usr/share/applications/wallpaper-select.desktop
