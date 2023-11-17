929a48fba00ea86187bac36bda13954e567816c5
Mato <96366252+Mato1111@users.noreply.github.com>
2023-11-17 18:17:02 -0500

    Update add-to-apps.sh

--- add-to-apps.sh
+++ add-to-apps.sh
@@ -7,4 +7,4 @@ cd ~/.local/share/applications/
 wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/Scripts/wallpaper-select.desktop
 
 sudo chmod +x /usr/bin/wallpaper-select
-sudo chmod +x /usr/share/applications/wallpaper-select.desktop
+chmod +x ~/.local/share/applications/wallpaper-select.desktop
