44607472147450d893fcc9b7d084324544ebb190
Mato <96366252+Mato1111@users.noreply.github.com>
2023-11-26 05:00:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash/
 
 mkdir ~/.config/WallpaperSelect/
+sleep 2
 cd ~/.config/WallpaperSelect/
 wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/Scripts/WallpaperSelect.py
 wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/Scripts/WallpaperEngineCustom.py
